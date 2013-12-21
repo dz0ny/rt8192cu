59d5b8e33c6c92a91c67946ac71e94901f47c005
vincent-t <vincentthiele@googlemail.com>
2013-12-21 12:43:12 +0100

    Update install.sh
    added blacklist for native kernel drivers

--- install.sh
+++ install.sh
@@ -7,4 +7,10 @@ sudo dkms build -m 8192cu -v 4.0.2.9000.20130911
 sleep 5
 sudo dkms install -m 8192cu -v 4.0.2.9000.20130911
 sleep 5
+echo "" | sudo tee -a /etc/modprobe.d/blacklist.conf
+echo "# Blacklist native RealTek 8188CUs drivers" | sudo tee -a /etc/modprobe.d/blacklist.conf   
+echo "blacklist rtl8192cu" | sudo tee -a /etc/modprobe.d/blacklist.conf   
+echo "blacklist rtl8192c_common" | sudo tee -a /etc/modprobe.d/blacklist.conf   
+echo "blacklist rtlwifi" | sudo tee -a /etc/modprobe.d/blacklist.conf
+sleep 5
 echo "RESTART YOUR SYSTEM PLEASE!"
