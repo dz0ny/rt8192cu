a205cc644fb743f5a651ec878717081c3728738f
vincent-t <vincentthiele@googlemail.com>
2014-09-22 13:11:16 +0200

    Update remove_driver.sh
    no reboot needed anymore

--- remove_driver.sh
+++ remove_driver.sh
@@ -3,5 +3,9 @@ echo "Remove rt8192cu..."
 sudo dkms remove -m 8192cu -v 4.0.2.9000.20130911 --all
 echo "Remove: /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf..."
 sudo rm /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
-read -p "Press [Enter] to reboot your PC..." cd
-sudo reboot
+sleep 5
+echo "Unload wlan drivers..."
+sudo rmmod 8192cu
+sleep 5
+echo "Load wlan drivers..."
+sudo modprobe rtl8192cu
