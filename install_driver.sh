868833db099f722e31167fe0fab4f0df82af584c
vincent-t <vincentthiele@googlemail.com>
2014-11-06 10:29:46 +0100

    Update install_driver.sh

--- install_driver.sh
+++ install_driver.sh
@@ -10,15 +10,11 @@ sudo dkms install -m 8192cu -v 4.0.2.9000.20130911
 sleep 5
 echo "Create: /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf..."
 sudo touch /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
-echo "# Blacklist native RealTek 8192CU-Drivers" | sudo tee -a /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
+echo "# Blacklist native RealTek 8192CU-Driver" | sudo tee -a /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
 echo "blacklist rtl8192cu" | sudo tee -a /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
-echo "blacklist rtl8192c_common" | sudo tee -a /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
-echo "blacklist rtlwifi" | sudo tee -a /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
 sleep 5
 echo "Unload wlan drivers..."
 sudo rmmod rtl8192cu
-sudo rmmod rtl8192c_common
-sudo rmmod rtlwifi
 sleep 5
 echo "Load wlan drivers..."
 sudo modprobe 8192cu
