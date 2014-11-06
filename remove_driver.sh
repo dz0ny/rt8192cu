db0a83a02f36fd41fd0af8f95787560b2c1e2239
vincent-t <vincentthiele@googlemail.com>
2014-11-06 10:19:25 +0100

    Update remove_driver.sh
    Remove src.

--- remove_driver.sh
+++ remove_driver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Remove rt8192cu..."
 sudo dkms remove -m 8192cu -v 4.0.2.9000.20130911 --all
+sudo rm -r -d /usr/src/8192cu-4.0.2.9000.20130911
 echo "Remove: /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf..."
 sudo rm /etc/modprobe.d/blacklist-rtl-wlan-drivers.conf
 sleep 5
